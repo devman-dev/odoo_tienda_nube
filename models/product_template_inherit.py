de6e2c53d96ccf72b5be6371aeb511067f131b1f
root <admin@autodidactati.com>
2025-07-21 19:35:32 -0300

    [ADD] Aaction update product price

--- models/product_template_inherit.py
+++ models/product_template_inherit.py
@@ -233,6 +233,14 @@ class TiendaNubeProductTemplateInherit(models.Model):
                 company = self.env.company if self.env.company else self.env.user.company_id
                 company.update_product_tn(product)
 
+    # Metodo de actualizacion de precio desde Odoo a TN
+    def update_price_tn(self):
+        for product in self:
+            if product.id_tn:
+                # NOTE: Utilizamos la compañia que tiene seleccionada el usuario actual o en caso contrario la compañia predeterminada de ese usuario
+                company = self.env.company if self.env.company else self.env.user.company_id
+                company.update_product_price_tn(product)
+
     # Metodo de actualizacion de stock desde Odoo a TN
     def update_stock_tn(self):
         for product in self:
