c7f13cdf561ce96794df433abedc67f79d4ef5c2
root <admin@autodidactati.com>
2025-07-31 19:25:59 -0300

    [ADD] Sincronizacion prod existentes TN <-> Odoo

--- wizards/__init__.py
+++ wizards/__init__.py
@@ -6,3 +6,4 @@ from . import create_all_webhook_odoo
 from . import create_all_coupon_odoo
 from . import create_all_order_odoo
 from . import get_tn_location_wizard
+from . import synchronize_products_wizard
