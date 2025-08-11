9c830f3412d9a725bc5c92cd8978d9fb96e53111
root <admin@autodidactati.com>
2025-08-11 16:54:47 -0300

    [FEAT] Mass Synchronize Product TN - Odoo

--- __manifest__.py
+++ __manifest__.py
@@ -4,7 +4,7 @@
     "summary": """
         Conector de Tienda Nube con Odoo""",
     "category": "Sale",
-    "version": "16.0.1",
+    "version": "16.0.2",
     "website": "https://devoo.io",
     "author": "Iván Arriola | Valentin Romero - Devoo",
     "license": "LGPL-3",
